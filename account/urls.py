a3395669b42a3a8d3e89e1d810d2db3d7f68b598
xc <727790922@qq.com>
2018-03-23 17:41:09 +0800

    add password_form

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout, password_change, password_change_done
 from django.contrib.auth.views import logout_then_login
 
 from account.views import dashboard
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^logout/$',logout,name='logout'),
     url(r'^logout-then-login/$',logout_then_login,name='logout_then_login'),
     url(r'^$',dashboard,name='dashboard'),
+    url(r'^password-change/$',password_change,name='password_change'),
+    url(r'^password-change/done/$',password_change_done,name='password_change_done'),
+
 ]
