251f7abbf99b20e9a2ce3be632a2566a6ddadc38
xc <727790922@qq.com>
2018-03-28 14:28:02 +0800

    add register

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, password_change, password_change_done
+from django.contrib.auth.views import login, logout, password_change, password_change_done, password_reset,password_reset_done
 from django.contrib.auth.views import logout_then_login
 
-from account.views import dashboard
+from account.views import dashboard,register
 
 urlpatterns = [
     url(r'^login/$',login,name='login'),
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^$',dashboard,name='dashboard'),
     url(r'^password-change/$',password_change,name='password_change'),
     url(r'^password-change/done/$',password_change_done,name='password_change_done'),
-    # url(r'^password-reset/$',)
-
+    url(r'^password-reset/$',password_reset,name='password_reset'),
+    url(r'^password-reset/done/$',password_reset_done,name='password_reset_done')
+    url(r'^register/$',register,name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,7 +2,27 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from account.form import UserRegistrationForm
+
 
 @login_required
 def dashboard(request):
     return render(request,'registration/dashboard.html',{'section':'dashboard'})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+            return render(request,
+                          'registration/register_done.html',
+                          {'new_user': new_user})
+        else:
+            user_form = UserRegistrationForm()
+        return render(request,
+                      'registration/register.html',
+                      {'user_form': user_form})
