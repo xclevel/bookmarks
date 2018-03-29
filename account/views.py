d8e11469082bb97a391bea002d46d20df9360310
xc <727790922@qq.com>
2018-03-29 17:14:48 +0800

    add setting.auth.user.model

--- account/views.py
+++ account/views.py
@@ -33,11 +33,11 @@ def register(request):
 def edit(request):
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user,data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user,data=request.POST,files=request.FILES)
+        profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
     return render(request,'registration/edit.html',{'user_form': user_form,'profile_form': profile_form})
